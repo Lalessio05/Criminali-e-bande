d0e35a4f175e5283f316d661bf87b9ae4b20a903
Alessio Catrini <catrini.alessio2@gmail.com>
2024-03-18 14:54:05 +0100

    Rimosso Progetto

---
+++ Bilboteche/Autori con media libri inferiori a media Rick.sql
@@ -0,0 +1,14 @@
+﻿SELECT AVG(Libro.Prezzo) as 'Media', Autore.Cognome, Autore.Nome
+FROM Autore
+INNER JOIN ScrittoDa on ScrittoDa.IdAutore_fk = Autore.IdAutore
+INNER JOIN Libro on Libro.IdLibro = ScrittoDa.IdLibro_fk
+GROUP BY Autore.Nome, Autore.Cognome
+HAVING AVG(Libro.Prezzo) <
+	(
+	SELECT AVG(Libro.Prezzo)
+	FROM Libro
+	INNER JOIN ScrittoDa on ScrittoDa.IdLibro_fk = Libro.IdLibro
+	INNER JOIN Autore on Autore.IdAutore = ScrittoDa.IdAutore_fk
+	WHERE (Autore.Nome = 'Rick')
+)
+
